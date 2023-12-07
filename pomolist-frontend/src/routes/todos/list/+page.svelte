3232e10f231aff78cadfdda0c46971e8c9af83ac
mwidman <2118673+mwidman@users.noreply.github.com>
2023-12-06 18:41:09 -0800

    style: Basic styling with dark mode.

--- pomolist-frontend/src/routes/todos/list/+page.svelte
+++ pomolist-frontend/src/routes/todos/list/+page.svelte
@@ -4,37 +4,31 @@
 
 <ul>
 {#each data.todos as todo}
-    <li><a href='/todos/{todo.id}'>{todo.title}</a></li>
+    <li class="card w-full shadow-md my-8"><a href='/todos/{todo.id}'>{todo.title}</a></li>
 {/each}
 </ul>
 
+<hr class="w-full" />
 <form method="POST" action="?/create">
-    <label>
-        Title:
-        <input name="title" type="text" required />
-    </label>
-    <label>
-        Priority:
-        <select name="priority">
-            <option value="1">P1</option>
-            <option value="2">P2</option>
-            <option value="3">P3</option>
-            <option value="4">P4</option>
-        </select>
-    </label>
+    <div class="flex flex-row justify-between items-center">
+        <label class="w-1/36" for="todo-title">Title:</label>
+        <input id="todo-title" class="inline-block input w-full max-w-lg" name="title" type="text" required placeholder="Short Description" />
+        <label>
+            Priority:
+            <select class="select" name="priority">
+                <option value="1">P1</option>
+                <option value="2">P2</option>
+                <option value="3">P3</option>
+                <option value="4">P4</option>
+            </select>
+        </label>
+        </div>
     <label>
         Description:
-        <textarea name="description"></textarea>
+        <textarea class="textarea w-full" name="description" placeholder="Write details here..."></textarea>
     </label>
-    <button type="submit">Create</button>
+    <button class="btn w-full" type="submit">Create</button>
 </form>
 
-<style>
-    li {
-        list-style: none;
-        border-radius: 0.5em;
-        border: 1px solid #000;
-        padding: 0.5em;
-        margin-bottom: 0.5em;
-    }
+<style lang="postcss">
 </style>
